cb3ba75d7f0758f1e1f753185406a8317ca3b014
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2024-06-20 18:25:31 +0700

    [agus]feat: add bottom navigation & stack navigation

---
+++ src/screens/progateEvent.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function ProgateEvent() {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title, styles.marginBottom20]}>Progate Event</Text>
+      <Text>Join Progate Event to learn coding!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+  marginBottom20: {
+    marginBottom: 20,
+  },
+});
