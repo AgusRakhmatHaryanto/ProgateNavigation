c44241d1e28105135937e697d852134a0df9414b
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2024-06-20 19:29:43 +0700

    [agus]feat: add useEffect alert

--- src/screens/progateEvent.js
+++ src/screens/progateEvent.js
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default function ProgateEvent() {
+  useEffect(() => {
+    alert('ProgateEvent screen is mounted')
+    return () => {
+      alert('ProgateEvent screen is unmounted')
+    }
+  })
   return (
     <View style={styles.container}>
       <Text style={[styles.title, styles.marginBottom20]}>Progate Event</Text>
